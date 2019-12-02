63f28fd5f99ae29e6a4f4edfc250629173b0ad3c
TAKUMA Katou <katou.physics@gmail.com>
2019-12-02 16:50:51 +0900

    add opencv

--- gui/lifegame.py
+++ gui/lifegame.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[127]:
+# In[12]:
 
 
 import numpy as np
 import tkinter as tk
 from multiprocessing import Pool
+import cv2
 
 
-# In[137]:
+# In[14]:
 
 
 class cells:
     def __init__(self,m,n,w,canvas):
         #初期セル配置は常にゼロ
         #selfのセルにデータを格納
-        self.cell=np.zeros((m+1,n+1),dtype=int)
-        self.ncell=np.zeros((m+1,n+1),dtype=int)
+        self.cell=np.zeros((m+2,n+2),dtype=np.uint8)
+        self.ncell=np.zeros((m+2,n+2),dtype=np.uint8)
         self.height=m
         self.width=n
         self.cellsize=w
         self.canvas=canvas
+        self.kernel = np.array([[1,1,1],[1,0,1],[1,1,1]], dtype=np.uint8)
+
         #運命関数の定義
         #self.dest=np.zeros((256,2),dtype=int)
-        self.dest=self.fate()
+        #self.dest=self.fate()
         #for i in range(256):
         #    print(i,self.dest[i])
     #初期条件として左上にグライダーを設定
@@ -39,44 +42,45 @@ class cells:
         y1=event.y//self.cellsize
         #描画
         tag='cell'+str(self.height*x1+y1)
-        if self.cell[x1,y1]==0:
+        if self.cell[x1+1,y1+1]==0:
             self.canvas.create_rectangle(self.cellsize*x1, self.cellsize*y1, self.cellsize*x1+self.cellsize, self.cellsize*y1+self.cellsize,outline='black',tag=tag,fill='lime', width=1)
-            self.cell[x1,y1]=1
+            self.cell[x1+1,y1+1]=1
         else:
             self.canvas.delete(tag)
-            self.cell[x1,y1]=0
-    def fate(self):
-        d=np.zeros((256,2),dtype=int)
-        for i in range(256):
-            a=np.zeros(8,dtype=int)
-            c=np.zeros(3,dtype=int)
-            n=i
-            j=1
-            #a=0
-            while n>0:
-                a[8-j]=n%2
-                n=n//2
-                j=j+1
-            b=np.sum(a)
-            n=b
-            j=1
-            #c=0
-            while n>0:
-                c[3-j]=n%2
-                n=n//2
-                j=j+1
-            #determine fate
-            d[i,0]=c[1]*c[2]*(-c[0]+1)
-            d[i,1]=(-c[0]+1)*c[1]
-        return d    
+            self.cell[x1+1,y1+1]=0
+   # def fate(self):
+   #     d=np.zeros((256,2),dtype=int)
+   #     for i in range(256):
+   #         a=np.zeros(8,dtype=int)
+   #         c=np.zeros(3,dtype=int)
+   #         n=i
+   #         j=1
+   #         #a=0
+   #         while n>0:
+   #             a[8-j]=n%2
+   #             n=n//2
+   #             j=j+1
+   #         b=np.sum(a)
+   #         n=b
+   #         j=1
+   #         #c=0
+   #         while n>0:
+   #             c[3-j]=n%2
+   #             n=n//2
+   #             j=j+1
+   #         #determine fate
+   #         d[i,0]=c[1]*c[2]*(-c[0]+1)
+   #         d[i,1]=(-c[0]+1)*c[1]
+   #     return d    
     def step(self):
-        for j in range(self.width):
-            for i in range(self.height):
-                a=128*self.cell[i-1,j-1]+64*self.cell[i-1,j]+32*self.cell[i-1,j+1]+16*self.cell[i,j+1]+8*self.cell[i+1,j+1]+4*self.cell[i+1,j]+2*self.cell[i+1,j-1]+self.cell[i,j-1]
-                self.ncell[i,j]=self.dest[a,self.cell[i,j]]
-                #print(a)
+        a=cv2.filter2D(self.cell, -1, self.kernel)
+        #print(a)
+        alive = (self.cell == 1) * ((a == 2) + (a == 3))*1
+        born  = (self.cell == 0) * (a == 3)*1
         
-        self.cell=self.ncell.copy()
+        self.ncell=alive+born
+        
+        self.cell=self.ncell.astype(np.uint8)
         #for j in range(self.width):
         #    for i in range(self.height):
         #        self.cell[i,j]=self.ncell[i,j]
@@ -88,14 +92,6 @@ class cells:
             for i in range(self.height):
                 if self.cell[i,j]==1:
                      self.canvas.create_rectangle(self.cellsize*i, self.cellsize*j, self.cellsize*i+self.cellsize, self.cellsize*j+self.cellsize,outline="black",fill='lime', width=1)            
-            
-                
-                
-                
-                
-                
-                
-    
 
 
 # In[111]:
@@ -117,10 +113,17 @@ class cells:
 #a=cells(m,n,wcell,canvas)
 
 
-# In[133]:
+# In[31]:
 
 
-
+#m=10
+#n=10
+#cell=np.ones((3,3),np.float32)
+#kernel = np.array([[1,1,1],[1,0,1],[1,1,1]], dtype=int)
+#a=cv2.filter2D(cell, -1, kernel)
+#print(a)
+#a=a.astype(np.int)
+#print(a)
 
 
 # In[ ]:
